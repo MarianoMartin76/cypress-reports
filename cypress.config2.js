b85c06513ded08e7c4a60ea8ec43633b7c093226
Mariano Martin <mariano.martin76@gmail.com>
2025-06-17 20:32:52 -0300

    :alembic: fix tests

--- cypress.config2.js
+++ cypress.config2.js
@@ -2,25 +2,20 @@ const { defineConfig } = require("cypress");
 const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
-  video: false,
-  videoUploadOnPasses: false,
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
     charts: true,
-    reportPageTitle: 'custom-title',
+    reportPageTitle: "custom-title",
     embeddedScreenshots: true,
     inlineAssets: true,
     saveAllAttempts: false,
   },
   e2e: {
-    baseUrl: "https://petstore.swagger.io/",
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
       allureWriter(on, config);
       return config;
-    },
-    env: {
-      allureReuseAfterSpec: true
     },
   },
 });
--- cypress/e2e/UI-specs.cy.js
+++ cypress/e2e/UI-specs.cy.js
@@ -1,16 +1,17 @@
 /// <reference types="Cypress" />
 
-describe('Test UI', () => {
-    beforeEach(() => {
-        cy.visit('https://petstore.swagger.io/');
-    });     
-    it('url should match', () => {
-        cy.url().should('eq', 'https://petstore.swagger.io/')
-    });
-    it('title should match', () => {
-        cy.get('.title').should('contain', 'Swagger Petstore')
-    });
-    it('version should match', () => {
-        cy.get('.version').should('contain', '1.0.6');
-    });
-})
+describe("Test UI", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("url should match", () => {
+    cy.url().should("eq", "https://petstore.swagger.io/");
+  });
+  it("title should match", () => {
+    cy.get(".title").should("contain", "Swagger Petstore");
+  });
+  it("version should match", () => {
+    cy.get('.ch2-allow-all-btn').click();
+    cy.get(".version").should("contain", "1.0.7");
+  });
+});
